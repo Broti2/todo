5a48acef1b5eea373b216a0af8bde9188a09cc6f
Broti2 <brotisunder9@gmail.com>
2024-06-07 18:23:28 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,99 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri =
+  'mongodb+srv://broti:Broti143@cluster0.bjvjbsy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// MongoDB connection
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+app.get('/api/list', async (req, res) => {
+  try {
+    const collection = client.db('sampledata').collection('Todo');
+    const list = await collection.find({}).toArray();
+    res.json(list);
+  } catch (error) {
+    console.error('Error fetching todo list:', error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
+
+// Route to add a new todo
+app.post('/api/todo', async (req, res) => {
+  const todo = req.body;
+  console.log(todo);
+  var dbo = client.db('sampledata');
+  var myobj = { text: todo.todo, complete: false };
+  dbo.collection('Todo').insertOne(myobj, function (err, res) {
+    if (err) throw err;
+    console.log('1 document inserted');
+    db.close();
+  });
+});
+
+app.delete('/api/delete/:id', async (req, res) => {
+  try {
+    const id = req.params.id; // Extract id from req.params
+    console.log(id);
+    var dbo = client.db('sampledata');
+    const result = await dbo
+      .collection('Todo')
+      .deleteOne({ _id: new ObjectId(id) });
+    console.log('Deleted:', result);
+    res.json({ message: 'Deleted Successfully' });
+  } catch (error) {
+    console.error('Error deleting todo:', error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
+
+app.patch('/api/patch/:id', async (req, res) => {
+  try {
+    const id = req.params.id; // Extract id from req.params
+    console.log(id);
+    var dbo = client.db('sampledata');
+    const result = await dbo
+      .collection('Todo')
+      .updateOne({ _id: new ObjectId(id) }, { $set: { complete: true } });
+    console.log('Updated:', result);
+    res.json({ message: 'Updated Successfully' });
+  } catch (error) {
+    console.error('Error updating todo:', error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
+
+app.put('/api/update/:id', async (req, res) => {
+  try {
+    const id  = req.params.id;
+    const  {todo}  = req.body;
+    console.log(id);
+    console.log(todo);
+    const dbo = client.db('sampledata');
+    const result = await dbo.collection('Todo').updateOne(
+      { _id: new ObjectId(id) },
+      { $set: { text: todo } }
+    );
+    console.log('Updated:', result);
+    res.json({ message: 'Updated Successfully' });
+  } catch (error) {
+    console.error('Error updating todo:', error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
---
+++ frontend/app/page.tsx
@@ -0,0 +1,196 @@
+'use client';
+import EditNoteIcon from '@mui/icons-material/EditNote';
+import { useEffect, useState } from 'react';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import axios from 'axios';
+
+export default function Home() {
+  debugger;
+  const [list, setList] = useState([]);
+  const [todo, setTodo] = useState('');
+  const [hidden, setHidden] = useState({ id: '', hidden: '' });
+  const [update, setUpdate] = useState('');
+  const fetch = async () => {
+    const data = await axios.get('http://localhost:5000/api/list');
+    setList(data.data);
+    console.log(data.data);
+  };
+  useEffect(() => {
+    fetch();
+  }, []);
+
+  const del = async (e: any) => {
+    try {
+      const text = e;
+      console.log(text);
+      await axios.delete(`http://localhost:5000/api/delete/${text}`);
+      console.log('Successfully deleted');
+      // Fetch the updated list after deletion
+      await fetch();
+    } catch (error) {
+      console.error('Error deleting todo:', error);
+    }
+  };
+
+  const Add = async (e: any) => {
+    e.preventDefault();
+    try {
+      await axios.post('http://localhost:5000/api/todo', { todo: todo });
+      console.log('Successfully added todo');
+      setTodo(''); // Clear input value
+      // Fetch the updated list after addition
+      fetch();
+    } catch (error) {
+      console.error('Error adding todo:', error);
+    }
+  };
+
+  const updatetodo = async () => {
+    try {
+      await axios
+        .put(`http://localhost:5000/api/update/${hidden.id}`, {
+          todo: update,
+        })
+        .then(() => console.log('Successfully added todo'));
+
+      setUpdate(''); // Clear input value
+      // Fetch the updated list after addition
+      setHidden({ id: '', hidden: '' });
+      fetch();
+    } catch (error) {
+      console.error('Error adding todo:', error);
+    }
+  };
+
+  return (
+    <>
+      <div>
+        <h1 className="text-white py-5 text-5xl font-semibold text-center">
+          Todo List
+        </h1>
+        <div className="py-5">
+          <div className="h-16   flex justify-center">
+            <form className="h-full " onSubmit={Add}>
+              <input
+                onChange={(e) => setTodo(e.target.value)}
+                value={todo}
+                className="h-full w-64 text-black text-2xl px-2"
+                placeholder="Add todo"
+                type="text"
+              />{' '}
+              <button
+                className="h-full bg-slate-700 w-24 text-2xl font-bold"
+                type="submit"
+              >
+                Add
+              </button>
+            </form>
+          </div>
+          <div className="py-5">
+            {list.length === 0 ? (
+              <div className="flex justify-center italic">
+                <h2>No record</h2>
+              </div>
+            ) : (
+              list.map((e: any) => (
+                <div
+                  className="mx-auto flex justify-between my-3 py-3 rounded-lg text-2xl px-5 w-[25%] bg-white text-black"
+                  key={e._id}
+                >
+                  <div
+                    className={hidden.id === e._id ? hidden.hidden : 'w-5 h-5'}
+                  >
+                    {e.complete === true ? (
+                      <input
+                        checked
+                        readOnly
+                        className="rounded-full w-5 h-5"
+                        type="checkbox"
+                      />
+                    ) : (
+                      <input
+                        onClick={async () => {
+                          await axios
+                            .patch(`http://localhost:5000/api/patch/${e._id}`)
+                            .then(() => console.log('hero'))
+                            .catch((error) => console.log(error));
+                          fetch();
+                        }}
+                        className={
+                          'rounded-full w-5 h-5  ' + hidden.id === e._id
+                            ? hidden.hidden
+                            : 'w-5 h-5'
+                        }
+                        type="checkbox"
+                      />
+                    )}
+                  </div>
+                  <div
+                    className={
+                      e.complete === true
+                        ? 'line-through'
+                        : hidden.id === e._id
+                        ? hidden.hidden
+                        : ''
+                    }
+                  >
+                    {e.text}
+                  </div>
+                  <div
+                    className={
+                      'flex ' + hidden.id === e._id ? hidden.hidden : ''
+                    }
+                  >
+                    <button
+                      className={hidden.id === e._id ? hidden.hidden : ''}
+                      onClick={() => {
+                        setHidden({ id: e._id, hidden: 'hidden' });
+                        setUpdate(e.text);
+                      }}
+                      type="button"
+                    >
+                      <EditNoteIcon />
+                    </button>{' '}
+                    <button
+                      className={hidden.id === e._id ? hidden.hidden : ''}
+                      onClick={() => del(e._id)}
+                      type="button"
+                    >
+                      <DeleteForeverIcon />
+                    </button>
+                  </div>
+                  <div
+                    className={
+                      hidden.id === e._id ? ' h-full float-left flex' : 'hidden'
+                    }
+                  >
+                    <form
+                      className="flex"
+                      onSubmit={(e) => {
+                        e.preventDefault();
+                        updatetodo();
+                      }}
+                    >
+                      <input
+                        className="w-52 h-10 px-2 mx-4 border-2 border-black"
+                        onChange={(e) => setUpdate(e.target.value)}
+                        value={update}
+                        type="text"
+                      />{' '}
+                      <button
+                        type="submit"
+                        className="text-lg font-semibold px-3  -my-1 rounded-lg bg-slate-600 text-white"
+                      >
+                        Update
+                      </button>
+                    </form>
+                  </div>
+                </div>
+              ))
+            )}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
